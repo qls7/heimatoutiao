5bcb47d79b863dd23ae23082ad8db97659f1e378
qls7 <qls7@protonmail.com>
2019-07-17 21:04:06 +0800

    make sqlalchemy read-write-separater

--- common/models/__init__.py
+++ common/models/__init__.py
@@ -1,4 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 
 # 创建数据库连接对象
-db = SQLAlchemy()
+# db = SQLAlchemy()
+# 使用自己创建的sqlalchemy
+from .db_routing.routing_sqlalchemy import RoutingSQLAlchemy
+
+db = RoutingSQLAlchemy()
