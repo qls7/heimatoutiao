39926d04925d8d1ba736f9ec5b72849632412170
qls7 <qls7@protonmail.com>
2019-07-19 21:53:46 +0800

    deal cache exception

--- common/cache/user.py
+++ common/cache/user.py
@@ -1,6 +1,8 @@
 import pickle
 
 from flask import current_app
+from redis import RedisError
+from sqlalchemy.exc import DatabaseError
 from sqlalchemy.orm import load_only
 
 from cache.constants import UserNotExistCacheTTL, UserProfileCacheTTL
@@ -19,27 +21,34 @@ class UserProfileCache(object):
     def get(self):
         """获取缓存数据"""
         # 先进行获取缓存数据
-        # 如果有数据进行判断是不是-1 防止缓存穿透设置的
-        # 如果不是-1 直接进行返回
-        user_data = self.cluster.get(self.key)
-        # if user_data and user_data != b'-1':
-        #     return pickle.loads(user_data)
-        # else:
-        #     user = None
-        # 击中缓存
+        try:
+            user_data = self.cluster.get(self.key)
+        except RedisError as e:
+            current_app.logger.error(e)
+            user_data = None
+
         if user_data:
+            # 如果有数据进行判断是不是-1 防止缓存穿透设置的
             if user_data == b'-1':
                 return None
             else:
+                # 如果不是-1 直接进行返回
                 return pickle.loads(user_data)
         else:
             # 未击中缓存,去mysql数据库查询
-            user = User.query.options(load_only(User.name, User.mobile,
+            try:
+                user = User.query.options(load_only(User.name, User.mobile,
                                                 User.profile_photo, User.certificate,
                                                 User.introduction)).filter(id=self.user_id).first()
+            except DatabaseError as e:
+                current_app.logger.error(e)
+                raise e
             # 防止缓存穿透
             if not user:
-                self.cluster.setex(self.key, UserNotExistCacheTTL.get_val(), -1)
+                try:
+                    self.cluster.setex(self.key, UserNotExistCacheTTL.get_val(), -1)
+                except RecursionError as e:
+                    current_app.logger.error(e)
                 return None
 
             user_dict = {
@@ -51,7 +60,10 @@ class UserProfileCache(object):
             }
             user_str = pickle.dumps(user_dict)
             # 保存到缓存
-            self.cluster.setex(self.key, UserProfileCacheTTL.get_val(), user_str)
+            try:
+                self.cluster.setex(self.key, UserProfileCacheTTL.get_val(), user_str)
+            except RecursionError as e:
+                current_app.logger.error(e)
 
             # 进行返回
             return user_dict
