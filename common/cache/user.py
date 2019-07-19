2cc09254bc9ec358a102aa51e378f0448693928c
qls7 <qls7@protonmail.com>
2019-07-19 22:26:15 +0800

    data get(),save() and exist() little fix

--- common/cache/user.py
+++ common/cache/user.py
@@ -13,6 +13,7 @@ class UserProfileCache(object):
     """
     用户基本信息缓存类,每个缓存对象,对应一套用户缓存数据
     """
+
     def __init__(self, user_id):
         self.user_id = user_id  # 用户id
         self.key = 'user:{}:profile'.format(self.user_id)  # redis中存储的键
